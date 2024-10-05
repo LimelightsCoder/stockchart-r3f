e389837ab459f211f3cdba8929f588a212ad9776
LimelightsCoder <cory.komaguchi@gmail.com>
2024-10-04 21:53:26 -0700

    edits

--- src/components/dom/footer/index.jsx
+++ src/components/dom/footer/index.jsx
@@ -47,7 +47,7 @@ const Footer = () => {
   // JSX structure of the footer
   return (
     <div className='relative mx-auto py-16 px-4 grid  gap-8 text-gray-300 border-t select-none'>
-            <div className='absolute flex  top-[80vh] right-4'>
+            <div className='absolute flex  top-[90vh] right-4'>
             <svg width="5vw" height="5vh" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg"> <g clip-path="url(#clip0_116_153)"> <path d="M100 0C103.395 53.7596 146.24 96.6052 200 100C146.24 103.395 103.395 146.24 100 200C96.6052 146.24 53.7596 103.395 0 100C53.7596 96.6052 96.6052 53.7596 100 0Z" fill="url(#paint0_linear_116_153)"/> </g> <defs> <linearGradient id="paint0_linear_116_153" x1="100" y1="0" x2="100" y2="200" gradientUnits="userSpaceOnUse"> <stop stop-color="#ffffff"/> <stop offset="1" stop-color="#ffffff"/> </linearGradient> <clipPath id="clip0_116_153"> <rect width="200" height="200" fill="white"/> </clipPath> </defs> </svg>
             </div>
       <div className='flex justify-start items-start mt-6'>

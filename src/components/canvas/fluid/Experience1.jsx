a4cb226a958763b4eea7492184178c3a3da77e7d
LimelightsCoder <cory.komaguchi@gmail.com>
2024-10-07 16:48:30 -0700

    edits

--- src/components/canvas/fluid/Experience1.jsx
+++ src/components/canvas/fluid/Experience1.jsx
@@ -15,6 +15,7 @@ import {
 import { Fluid } from './Fluid';
 
 const Experience1 = () => {
+  const isMobile = window.innerWidth <= 768;
   const Width = window.innerWidth;
   const Height = window.innerHeight;
 
@@ -22,20 +23,22 @@ const Experience1 = () => {
    <>
 
             <EffectComposer>
+            {!isMobile && (
+                <>
                 {/* <Noise opacity={0.05} /> */}
-                {/* <Pixelation
-            granularity={2} // pixel granularity
-          /> */}
+                <Pixelation
+            granularity={4} // pixel granularity
+          />
                 <Fluid 
                 curl={1}
-                radius={0.15}
+                radius={0.1}
                 force={10}  
                 swirl={1}
                 blend={10}
-                pressure={0.9}
+                pressure={0.92}
                 fluidColor='#ffffff'
                 //intensity={1.5}
-                densityDissipation={0.9}
+                densityDissipation={0.88}
                 velocityDissipation={0.9}
                 //backgroundColor='#000000' 
                 rainbow={false}
@@ -51,7 +54,30 @@ const Experience1 = () => {
                 blendFunction={BlendFunction.DIFFERENCE} 
                 middleGrey={0} 
                 maxLuminance={0} /> */}
-
+                 </>
+                )}
+                {isMobile && (
+                   <>
+                <Fluid 
+                curl={1}
+                radius={0.15}
+                force={5}  
+                swirl={1}
+                blend={10}
+                pressure={0.9}
+                fluidColor='#ffffff'
+                //intensity={1.5}
+                densityDissipation={0.9}
+                velocityDissipation={0.9}
+                //backgroundColor='#000000' 
+                rainbow={false}
+                showBackground={false}
+                />
+                   <Pixelation
+            granularity={4} // pixel granularity
+          />
+                   </>
+                  )}
             </EffectComposer>
    </>
   )
